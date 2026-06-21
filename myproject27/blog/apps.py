0d14887b325e40dac9c3cf3d3d0568ba8407ed26
Ahmad0175ahmad <ahmadbro175@gmail.com>
2026-06-21 16:13:20 +0500

    first commit

---
+++ myproject27/blog/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class BlogConfig(AppConfig):
+    name = 'blog'
+
+    def ready(self):
+        import blog.signals #imports signals to ensure they are register
